531e2a61c9392691840fa7a670399bc54afb030f
shakibul742 <shakibulislam74254285@gmail.com>
2026-07-26 02:18:25 +0600

    chore: Clean up temporary generation scripts

--- test.md
+++
@@ -1,5 +0,0 @@
-```ansi
-[31mRed text[0m
-[32mGreen text[0m
-[34mBlue text[0m
-```
--- test.py
+++
@@ -1,6 +0,0 @@
-with open("test.md", "w") as f:
-    f.write("```ansi\n")
-    f.write("\033[31mRed text\033[0m\n")
-    f.write("\033[32mGreen text\033[0m\n")
-    f.write("\033[34mBlue text\033[0m\n")
-    f.write("```\n")
